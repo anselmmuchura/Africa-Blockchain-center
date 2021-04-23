9d43ce7413d3dee696973d77f2d988fb58c16670
Anselm Muchura <32093513+anselmmuchura@users.noreply.github.com>
2021-04-23 14:52:33 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,26 @@
 !(function($) {
   "use strict";
 
+    $('#team-slider').on('slide.bs.carousel', function (e) {
+
+      var $e = $(e.relatedTarget);
+      var idx = $e.index();
+      var itemsPerSlide = 4;
+      var totalItems = $('.carousel-item').length;
+      
+      if (idx >= totalItems-(itemsPerSlide-1)) {
+          var it = itemsPerSlide - (totalItems - idx);
+          for (var i=0; i<it; i++) {
+              // append slides to end
+              if (e.direction=="left") {
+                  $('.carousel-item').eq(i).appendTo('.carousel-inner');
+              }
+              else {
+                  $('.carousel-item').eq(0).appendTo('.carousel-inner');
+              }
+          }
+      }
+  });
   
 
 })(jQuery);
